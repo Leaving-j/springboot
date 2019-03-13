1c457e196939165011b22e9ae5a938d32fa8f27b
Leewen <3936044601@qq.com>
2019-03-13 16:24:35 +0800

    1、druid数据源及log4j2

--- src/main/java/com/restful/controller/JspController.java
+++ src/main/java/com/restful/controller/JspController.java
@@ -1,22 +1,25 @@
 package com.restful.controller;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import com.restful.bo.Student;
 import com.restful.service.StuService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
-import java.lang.reflect.Type;
 import java.util.List;
 
 /**
@@ -40,14 +43,16 @@ public class JspController {
     @Autowired
     private RedisTemplate redisTemplate;
 
+
     @RequestMapping(value = "/list")
     public String list(Model mv) {
+        logger.info("----- 查询列表 -----");
         List<Student> students = this.stuService.findAllStudent();
         Gson gson = new Gson();
         String stuJson = gson.toJson(students);
 
         // 只能处理字符串
-        stringRedisTemplate.opsForValue().set("stuJson",stuJson);
+        stringRedisTemplate.opsForValue().set("stuJson", stuJson);
         String redisStr = stringRedisTemplate.opsForValue().get("stuJson");
 
         // 可以处理对象
@@ -57,4 +62,25 @@ public class JspController {
         mv.addAttribute("students", students);
         return "student";
     }
+
+    @GetMapping("/findOne/{id}")
+    @ResponseBody
+    public Student findStudentById(@PathVariable("id") int id) {
+        return this.stuService.findStudentByIdByMybatis(id);
+    }
+
+    @RequestMapping(value = "/rest")
+    public String rest(Model mv) {
+        RestTemplate restTemplate = new RestTemplateBuilder().build();
+        HttpHeaders headers = new HttpHeaders();
+        MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
+        headers.setContentType(type);
+        headers.add("Accept", MediaType.APPLICATION_JSON.toString());
+
+        String url = "http://localhost:8080/jsp/findOne/2";
+        Student student = restTemplate.getForObject(url, Student.class);
+
+        return "student";
+    }
+
 }
---
+++ src/main/webapp/WEB-INF/jsp/abc.md
@@ -0,0 +1,26 @@
+#这个还是有点叼的
+## 这是二级标题
+
+``` java
+ // 注释
+ 
+ private string
+
+```
+> 这是一块r
+
+*qindao*
+
+1. 这个就比较强大了
+2. 跳转到[百度](https://www.baidu.com)
+
+ 插入一下图片![github](https://upload-images.jianshu.io/upload_images/1293430-4362308e14d20dbb.gif)
+ 
+ tab1|tab2
+ :---------------:|:------------------:
+ 1|2
+ 1|2
+ 
+ >markdown
+   
+   
